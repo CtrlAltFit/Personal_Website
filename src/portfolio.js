4a397059cc535a766ad0e43ceb7a78c64ff7acd7
CtrlAltFit <elmehdi.benazzouz@esi.ac.ma>
2025-06-15 22:18:24 +0200

    test modifs in deployment 2

--- src/portfolio.js
+++ src/portfolio.js
@@ -315,18 +315,12 @@ const blogSection = {
   blogs: [
     {
       url: "/blogs/my-first-blog.html",
-      title: "Just Move",
+      title: "Just Move Forward",
       description: "A Philosophy Against Paralysis"
     },
     {
-      url: "https://blog.usejournal.com/create-a-google-assistant-action-and-win-a-google-t-shirt-and-cloud-credits-4a8d86d76eae",
-      title: "Win a Google Assistant Tshirt and $200 in Google Cloud Credits",
-      description:
-        "Do you want to win $200 and Google Assistant Tshirt by creating a Google Assistant Action in less then 30 min?"
-    },
-    {
       url: "https://medium.com/@saadpasta/why-react-is-the-best-5a97563f423e",
-      title: "Why REACT is The Best?",
+      title: "Why REACT i The Best?",
       description:
         "React is a JavaScript library for building User Interface. It is maintained by Facebook and a community of individual developers and companies."
     }
