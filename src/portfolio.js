0cbe2843bac98493b4640ef66531aeb0ff4391ed
CtrlAltFit <elmehdi.benazzouz@esi.ac.ma>
2025-07-06 23:46:34 +0200

    modify the displayMedimBlogs in the blog block

--- src/portfolio.js
+++ src/portfolio.js
@@ -319,7 +319,7 @@ const blogSection = {
   title: "Blogs",
   subtitle:
     "With Love for Developing cool stuff, I love to write and teach others what I have learnt.",
-  displayMediumBlogs: "true", // Set true to display fetched medium blogs instead of hardcoded ones
+  displayMediumBlogs: "false", // Set true to display fetched medium blogs instead of hardcoded ones
   blogs: [
     {
       url: "/blogs/my-first-blog.html",
